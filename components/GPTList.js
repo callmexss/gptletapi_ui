9f43ae630fbeb19f07cd77b61e923094454bea76
callmexss <callmexss@126.com>
2023-11-11 11:02:09 +0800

    perf: update style.

--- components/GPTList.js
+++ components/GPTList.js
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function GPTList() {
   const [groupedGPTs, setGroupedGPTs] = useState({});
@@ -20,7 +20,7 @@ export default function GPTList() {
         }, {});
         setGroupedGPTs(categories);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         // Handle errors as needed
       }
     };
@@ -33,70 +33,71 @@ export default function GPTList() {
   };
 
   return (
-    <div className='mb-6'>
-      {/* Render category buttons or links */}
-      <div className='flex flex-wrap space-x-4 mb-4'>
+    <div className="container mx-auto px-4">
+      <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6">
+        Find the BEST GPTs for You!
+      </h2>
+      <div className="flex flex-wrap justify-center gap-3 mb-8">
         {Object.keys(groupedGPTs).map((category) => (
           <button
             key={category}
             onClick={() => handleCategoryClick(category)}
-            className={`py-2 px-4 ${selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-transparent text-blue-700 hover:bg-blue-500 hover:text-white'} rounded transition`}
+            className={`py-2 px-3 sm:px-4 text-sm sm:text-lg ${
+              selectedCategory === category
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-blue-700 hover:bg-blue-500 hover:text-white"
+            } rounded transition duration-300 ease-in-out`}
           >
-            {category}
+            {category} ({groupedGPTs[category].length})
           </button>
         ))}
         <button
           onClick={() => setSelectedCategory(null)}
-          className="py-2 px-4 bg-transparent text-red-600 hover:bg-red-500 hover:text-white rounded transition"
+          className="py-2 px-3 sm:px-4 text-sm sm:text-lg bg-red-200 text-red-600 hover:bg-red-500 hover:text-white rounded transition duration-300 ease-in-out"
         >
           Clear Filter
         </button>
       </div>
-      {Object.keys(groupedGPTs).map((category) => {
-        // Filter gpts if a category is selected
-        if (selectedCategory && category !== selectedCategory) {
-          return null;
-        }
-        return (
-          <div key={category}>
-            <h3 className='text-2xl font-bold'>{category}</h3>
-            <div className='flex flex-wrap'>
-              {groupedGPTs[category].map((gpt) => {
-                const description = gpt.description || '';
-                const limitLength = 64;
-                const truncatedDescription =
-                  description.length > limitLength
-                    ? `${description.substring(0, limitLength)}...`
-                    : description;
-
-                return (
-                  <div key={gpt.id} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-1/5 p-2">
-                    <div className="bg-white rounded overflow-hidden shadow-lg h-[300px] flex flex-col">
-                      <div className="px-6 py-4 flex-grow">
-                        <div className="font-bold text-xl mb-2 truncate">{gpt.name}</div>
-                        <div className='item-center mb-4'>
-                          <img src={gpt.image_url} className='mx-auto w-16 h-16 rounded-full flex-shrink-0 bg-gray-100' alt={gpt.name}></img>
-                        </div>
-                        <p className="text-gray-700 text-sm">{truncatedDescription}</p>
-                      </div>
-                      <div className="px-6 py-4 flex justify-between">
-                        <Link
-                         href={gpt.link_url} passHref
-                         className="text-blue-500 hover:underline"
-                         target="_blank" 
-                         rel="noopener noreferrer"
-                        >
-                          Use
-                        </Link>
-                      </div>
+      {Object.keys(groupedGPTs).map((category) =>
+        selectedCategory && category !== selectedCategory ? null : (
+          <div key={category} className="mb-6">
+            <h3 className="text-xl sm:text-2xl font-semibold text-gray-800 mb-4">
+              {category} ({groupedGPTs[category].length})
+            </h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {groupedGPTs[category].map((gpt) => (
+                <div
+                  key={gpt.id}
+                  className="bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                >
+                  <div className="p-4">
+                    <div className="font-bold text-lg mb-2 truncate">
+                      {gpt.name}
                     </div>
+                    <img
+                      src={gpt.image_url}
+                      className="w-16 h-16 sm:w-20 sm:h-20 mx-auto rounded-full object-cover bg-gray-100 mb-3"
+                      alt={gpt.name}
+                    />
+                    <p className="text-gray-700 text-sm mb-4">
+                      {gpt.description}
+                    </p>
+                    <Link
+                      className="text-blue-500 hover:underline"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={gpt.link_url}
+                      passHref
+                    >
+                      Use
+                    </Link>
                   </div>
-                );
-              })}
+                </div>
+              ))}
             </div>
           </div>
-        );
-      })}
+        )
+      )}
     </div>
   );
 }
