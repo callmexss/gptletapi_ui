4b448b831562e7bd70f41315985bc400ee0094ea
callmexss <callmexss@126.com>
2023-11-12 16:55:52 +0800

    feat: add search support.

--- components/GPTList.js
+++ components/GPTList.js
@@ -8,11 +8,29 @@ import SubmitURLForm from "../components/SubmitURLForm";
 
 export default function GPTList() {
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredGPTs, setFilteredGPTs] = useState({});
   const { groupedGPTs, fetchGPTData } = useGPTData();
 
   useEffect(() => {
     fetchGPTData();
   }, [fetchGPTData]);
+
+  useEffect(() => {
+    const filtered = searchQuery ? Object.keys(groupedGPTs).reduce((acc, category) => {
+      const filteredGPTs = groupedGPTs[category].filter(gpt =>
+        gpt.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        gpt.description.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      // Only add the category to the accumulator if there are GPTs after filtering
+      if (filteredGPTs.length > 0) {
+        acc[category] = filteredGPTs;
+      }
+      return acc;
+    }, {}) : groupedGPTs;
+
+    setFilteredGPTs(filtered);
+  }, [searchQuery, groupedGPTs]);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -21,6 +39,15 @@ export default function GPTList() {
   return (
     <div className="container mx-auto px-4">
       <SubmitURLForm />
+      <div className="my-4">
+        <input
+          type="search"
+          placeholder="Search GPTs..."
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          className="w-full p-2 border rounded"
+        />
+      </div>
       <h2 className="text-2xl sm:text-3xl font-bold text-center mb-6">
         GPTs List
       </h2>
@@ -37,18 +64,20 @@ export default function GPTList() {
         selectedCategory={selectedCategory}
         onSelectCategory={setSelectedCategory}
       />
-      {Object.keys(groupedGPTs).map((category) =>
+      {Object.keys(filteredGPTs).map((category) =>
         selectedCategory && category !== selectedCategory ? null : (
-          <div key={category} className="mb-6">
-            <h3 className="text-xl sm:text-2xl font-semibold text-gray-800 mb-4">
-              {category} ({groupedGPTs[category].length})
-            </h3>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {groupedGPTs[category].map((gpt) => (
-                <GPTCard key={gpt.id} gpt={gpt} />
-              ))}
+          filteredGPTs[category].length > 0 ? ( // Check if the category has GPTs
+            <div key={category} className="mb-6">
+              <h3 className="text-xl sm:text-2xl font-semibold text-gray-800 mb-4">
+                {category} ({filteredGPTs[category].length})
+              </h3>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {filteredGPTs[category].map((gpt) => (
+                  <GPTCard key={gpt.id} gpt={gpt} />
+                ))}
+              </div>
             </div>
-          </div>
+          ) : null  // If no GPTs, do not render the category
         )
       )}
     </div>
