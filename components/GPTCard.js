3a02b8a849f687c31173af09dcd9a8b1f580d923
callmexss <callmexss@126.com>
2023-11-28 20:10:39 +0800

    perf: update style.

--- components/GPTCard.js
+++ components/GPTCard.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from 'react';
 import Link from 'next/link';
@@ -7,14 +7,20 @@ const GPTCard = ({ gpt }) => {
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 ease-in-out border border-gray-200 hover:border-blue-500">
       <div className="p-4">
-        <div className="font-bold text-lg mb-2 text-gray-800 truncate">{gpt.name}</div>
-        <img
-          src={gpt.image_url}
-          className="w-16 h-16 sm:w-20 sm:h-20 mx-auto rounded-full object-cover bg-gray-100 mb-3 border-2 border-blue-300"
-          alt={gpt.name}
-        />
-        <p className="text-gray-700 text-sm mb-4">{gpt.description}</p>
-        <Link className="text-blue-500 hover:text-blue-700 hover:underline inline-block bg-blue-100 rounded-full px-4 py-1 transition duration-300 ease-in-out" href={gpt.link_url} passHref>
+        <h3 className="font-bold text-lg mb-2 text-gray-800 truncate hover:text-blue-600">{gpt.name}</h3>
+        <div className="mx-auto overflow-hidden hover:scale-150 hover:border-blue-400 transition duration-300 ease-in-out">
+          <img
+            src={gpt.image_url}
+            className="w-16 h-16 sm:w-20 sm:h-20 rounded-full mx-auto bg-gray-100 mb-3 transition-transform duration-300 ease-in-out hover:scale-150"
+            alt={gpt.name}
+          />
+        </div>
+        <p className="text-gray-700 text-sm mb-4 hover:text-gray-900">{gpt.description}</p>
+        <Link 
+         className="text-blue-500 hover:text-white inline-block bg-blue-100 hover:bg-blue-500 rounded-full px-4 py-1 transition duration-300 ease-in-out shadow hover:shadow-md"
+         href={gpt.link_url}
+         passHref
+        >
           Use
         </Link>
       </div>
