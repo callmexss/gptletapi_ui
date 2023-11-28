9d3f34b3d7a229b24d1f9dbd756999e9e20bc8e1
callmexss <callmexss@126.com>
2023-11-28 23:14:09 +0800

    fix: load page issue.

--- components/GPTList.js
+++ components/GPTList.js
@@ -8,38 +8,41 @@ import GPTCard from "../components/GPTCard";
 export default function GPTList() {
   const [searchQuery, setSearchQuery] = useState('');
   const [gpts, setGpts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
   const [totalGPTs, setTotalGPTs] = useState(0);
   const [nextPageUrl, setNextPageUrl] = useState(null);
 
-  const loadGPTs = async () => {
-    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/gpts/?page=${currentPage}&search=${searchQuery}`;
+  const loadGPTs = async (url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/gpts/?page=1&search=${searchQuery}`) => {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch GPTs: ${response.status}`);
     }
     const data = await response.json();
     setTotalGPTs(data.count);
-    setGpts(prevData => currentPage === 1 ? data.results : [...prevData, ...data.results]);
+    setGpts(data.results);
     setNextPageUrl(data.next);
   };
 
   useEffect(() => {
     loadGPTs();
-  }, [currentPage, searchQuery]);
+  }, [searchQuery]);
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
-    setCurrentPage(1); // Reset to first page on new search
   };
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const handleLoadMore = () => {
+  const handleLoadMore = async () => {
     if (nextPageUrl) {
-      loadGPTs(nextPageUrl);
+      const response = await fetch(nextPageUrl);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch more GPTs: ${response.status}`);
+      }
+      const data = await response.json();
+      setGpts(prevGpts => [...prevGpts, ...data.results]);
+      setNextPageUrl(data.next);
     }
   };
 
@@ -70,14 +73,18 @@ export default function GPTList() {
           <GPTCard key={gpt.id} gpt={gpt} />
         ))}
       </div>
-      {nextPageUrl && (
+      {nextPageUrl ? (
         <button
           onClick={handleLoadMore}
           className="w-full py-2 my-2 bg-blue-500 text-white rounded hover:bg-blue-700"
         >
           Load More
         </button>
-      )}
+      ) : gpts.length > 0 ? (
+        <div className="w-full py-2 my-2 text-center text-gray-700">
+          You've reached the end of the list.
+        </div>
+      ) : null}
     </div>
   );
 }
